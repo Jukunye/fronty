c9d1dff58d9a23985c17cfc17e8880d7c2bbfe5e
Jukunye <jukunyes@gmail.com>
2024-02-12 22:19:26 +0300

    Dashboard

---
+++ src/containers/Dashboard/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>Dashdash</div>
+    // <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+    //   <Grid xs={6}>
+    //     <Item>1</Item>
+    //   </Grid>
+    //   <Grid xs={6}>
+    //     <Item>2</Item>
+    //   </Grid>
+    //   <Grid xs={6}>
+    //     <Item>3</Item>
+    //   </Grid>
+    //   <Grid xs={6}>
+    //     <Item>4</Item>
+    //   </Grid>
+    // </Grid>
+  );
+}
+
+export default Dashboard;
