482693708b731fbe66b237adc6f75936a9984d5f
Jukunye <jukunyes@gmail.com>
2024-02-18 08:40:30 +0300

    auth protected all routes

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -6,17 +6,23 @@ import WeighbridgeIn from "../containers/Weighbridge/WeighbridgeIn";
 import WeighbridgeOut from "../containers/Weighbridge/weighbridgeOut";
 import Tankfarm from "../containers/Tankfarm/Tankfarm";
 import Trucks from "../containers/Trucks/Trucks";
+import SignIn from "../components/forms/SignIn";
+import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/trucks" element={<Trucks />} />
-      <Route path="/weighbridgein" element={<WeighbridgeIn />} />
-      <Route path="/lab" element={<Lab />} />
-      <Route path="/tankfarm" element={<Tankfarm />} />
-      <Route path="/Weighbridgeout" element={<WeighbridgeOut />} />
-      <Route path="/addtruck" element={<AddTruck />} />
+      <Route path="/login" element={<SignIn />} />
+
+      <Route element={<AuthOutlet fallbackPath="/Login" />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/trucks" element={<Trucks />} />
+        <Route path="/weighbridgein" element={<WeighbridgeIn />} />
+        <Route path="/lab" element={<Lab />} />
+        <Route path="/tankfarm" element={<Tankfarm />} />
+        <Route path="/Weighbridgeout" element={<WeighbridgeOut />} />
+        <Route path="/addtruck" element={<AddTruck />} />
+      </Route>
     </Routes>
   );
 }
